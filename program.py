92a4eac9ca0fa4c3815d424550fd6758b27066a7
Payal-Sanduja <payalekta19@gmail.com>
2025-02-13 23:22:50 +0530

    Update program.py

--- program.py
+++ program.py
@@ -26,7 +26,27 @@ i=1
 while i<=n:
     fact=fact*i
     i=i+1
-print("The Factotal of the given number is:",fact)    
+print("The Factotal of the given number is:",fact)  
+
+#Program to check whether the student is pass or not
+marks=int(input("Enter your Marks:-"))
+if(marks>=33):
+    print("Student is Pass")
+else:
+    print("Student is fail")
+
+
+#Program to Calculate the Grade of the student
+m=int(input("enter The Marks"))
+if(m>=90 and m<=100):
+    print("grade is A")
+elif(m>=80 and m<90):
+    print("Grade is B")
+elif(m>=70 and m<80):
+    print("Grade is C")
+else:
+    print("Grade is D")
+    
 
 
 
