30c3f605b90a0e70c7e37944370df2fad82256b1
Payal-Sanduja <payalekta19@gmail.com>
2025-02-13 09:20:29 -0800

    Add files via upload

---
+++ program.py
@@ -0,0 +1,5 @@
+#Program to Calculate the average of 2 Numbers
+n1=int(input("Enter the first number "))
+n2=int(input("Enter the Second Number "))
+Average=(n1+n2)/2
+print("The avraeg of the Two Numbers is== ",Average)       
