61265877435b59a0e6c1555faf8549d9fdac346d
Payal-Sanduja <payalekta19@gmail.com>
2025-02-13 22:56:40 +0530

    Update program.py

--- program.py
+++ program.py
@@ -3,3 +3,10 @@ n1=int(input("Enter the first number "))
 n2=int(input("Enter the Second Number "))
 Average=(n1+n2)/2
 print("The avraeg of the Two Numbers is== ",Average)       
+
+#Program to Calculate the average of 3 Numbers
+n3=int(input("Enter the first number "))
+n4=int(input("Enter the Second Number "))
+n5=int(input("Enter the first number "))
+Avg=(n3+n4+n5)/3
+print("The average of the Three Numbers is== ",Avg)
