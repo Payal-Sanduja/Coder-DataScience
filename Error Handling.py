606fbefc5b1ed25548a382be22f5800bce0203a2
Payal-Sanduja <payalekta19@gmail.com>
2025-02-15 23:18:45 +0530

    Update Error Handling.py

--- Error Handling.py
+++ Error Handling.py
@@ -4,6 +4,22 @@ try:
     print(number)
 except:
     print("An Exception occured beacuse the number is notdeclared ")
+
+
+#use of finally keyword(When the variable is not declared)
+          
+try:
+  number> 3
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+finally:
+  print("The try...except block is finished")
+    
+    
+    
+    
     
 
           
