f8cd6461fa3eb8af86e76eb911a3cf4576be716a
Payal-Sanduja <payalekta19@gmail.com>
2025-02-15 23:14:47 +0530

    Update Error Handling.py

--- Error Handling.py
+++ Error Handling.py
@@ -1,1 +1,10 @@
+# Use of try and Except(When an variable is not defined)
+
+try:
+    print(number)
+except:
+    print("An Exception occured beacuse the number is notdeclared ")
+    
+
+          
 
