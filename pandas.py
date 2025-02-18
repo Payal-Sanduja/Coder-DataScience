42d0067ad49e327491aee6657e29aa5ae5365add
Payal-Sanduja <payalekta19@gmail.com>
2025-02-18 18:58:38 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -6,23 +6,23 @@ print(var)
 #create a series from the given list
 import pandas as pd
 a=[1,2,3,4,5,6,7]
-var=pd.Series(a)
-print(var)
-print("The minimum number among the given series is :-",var.min())
-print("The maximum number among the given series is :-",var.max())
-print("The mean of the given series is:-",var.mean())
-print(type(var))
+vara=pd.Series(a)
+print(vara)
+print("The minimum number among the given series is :-",vara.min())
+print("The maximum number among the given series is :-",vara.max())
+print("The mean of the given series is:-",vara.mean())
+print(type(vara))
 
 
 
 #Accessing the series Elements
-print("The 1st index value of var is :-",var[0])
-print("The 2nd index value of var is :-",var[1])
-print("The 3rd  index value of var is :-",var[2])
-print("The 4th index value of var is :-",var[3])
-print("The 5th index value of var is :-",var[4])
-print("The 6th index value of var is :-",var[5])
-print("The 7th index value of var is :-",var[6])
+print("The 1st index value of var is :-",vara[0])
+print("The 2nd index value of var is :-",vara[1])
+print("The 3rd  index value of var is :-",vara[2])
+print("The 4th index value of var is :-",vara[3])
+print("The 5th index value of var is :-",vara[4])
+print("The 6th index value of var is :-",vara[5])
+print("The 7th index value of var is :-",vara[6])
 
 
 # we can also change the label
@@ -53,22 +53,36 @@ print(var4)
  
 #use of loc method(accessing the specified no of rows)
 print(var3.loc[[0,1,2]])
-
-
 #create the csv file
-var4.to_csv("payal.csv",index=False)
+var5=vara.to_csv("payal.csv",index=False)
 
 #read the csv file
-var5=pd.read_csv("payal.csv")
-print(var5.head(1))
-print(var5.tail(1))
-print(var5.info())
-print(var5.describe())
+var6=pd.read_csv("payal.csv")
+print(var6.head(1))
+print(var6.tail(1))
+print(var6.info())
+print(var.describe())
+
+
+print("json file creation")
+# Create the json file
+
+
+
+var7=var3.to_json("payal11.json")
+# read the json file
+var8=pd.read_json("payal11.json")
+print(var8.head(2))
 
 
 
 
-var5=var.to_json("payal1.json")
-var6=pd.read_json("payal1.json")
-print(var6.head(2))
+
+
+
+
+
+
+
+
 
