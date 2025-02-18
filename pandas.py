e833af831e872c29c30b02ecb2e6faea7a27bc1a
Payal-Sanduja <payalekta19@gmail.com>
2025-02-17 22:21:32 -0800

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import json
+A={"marks1":[1,2,3,4],"marks":[2,3,4,56]}
+var=pd.DataFrame(A)
+print(var)
+var1=var.to_csv("payal.csv")
+var2=pd.read_csv("payal.csv")
+print(var2.head(1))
+print(var2.tail(3))
+var4=var.to_json("payal1.json")
+var5=pd.read_json("payal1.json")
+print(var5.head(2))
 
