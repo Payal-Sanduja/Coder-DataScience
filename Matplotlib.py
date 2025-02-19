fc4fc927e2e6be07023ac4998f3a79d8abf5ae33
Payal-Sanduja <payalekta19@gmail.com>
2025-02-18 22:13:44 -0800

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as mp
+#Create the pie chart along with its label,explode,shadow
+x=[1,2,3,4,5,6,7]
+l=["Apple","Mango","Papaya","Orange","Guvava","lemon","cherr"]
+e=[0,0,0,0.5,0,0,0]
+mp.pie(x,labels=l,startangle=90,explode=e,shadow=True)
+
+mp.show()
+#change the color by using colors parameter
+c=["hotpink","orange","black","blue","red","yellow","brown"]
+mp.pie(x,labels=l,startangle=90,explode=e,shadow=True,colors=c)
+
+mp.show()
 
