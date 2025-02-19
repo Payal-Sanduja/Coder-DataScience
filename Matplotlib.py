418a427daf7b4f90509dfb47c60989aaed433d89
Payal-Sanduja <payalekta19@gmail.com>
2025-02-18 22:15:31 -0800

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -4,11 +4,13 @@ x=[1,2,3,4,5,6,7]
 l=["Apple","Mango","Papaya","Orange","Guvava","lemon","cherr"]
 e=[0,0,0,0.5,0,0,0]
 mp.pie(x,labels=l,startangle=90,explode=e,shadow=True)
+mp.legend()
 
 mp.show()
 #change the color by using colors parameter
 c=["hotpink","orange","black","blue","red","yellow","brown"]
 mp.pie(x,labels=l,startangle=90,explode=e,shadow=True,colors=c)
+mp.legend()
 
 mp.show()
 
