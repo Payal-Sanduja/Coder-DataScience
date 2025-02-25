43ef7805a21d1d0483448c850a08fac94c1cd284
Payal-Sanduja <payalekta19@gmail.com>
2025-02-25 22:47:35 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,2 +1,65 @@
 #Machine learning basics
-Machine Learningprint( (ML) is a subset of artificial intelligence (AI) that enables computers to learn patterns from data and make decisions without explicit programming. It helps automate decision-making processes based on historical data)
+print("Machine Learning(ML) is a subset of artificial intelligence (AI) that enables computers to learn patterns from data and make decisions without explicit programming. It helps automatedecision-making processes based on historical data")
+
+print()
+print()
+print("Features of machine learning")
+print("Learning from Data: Algorithms analyze data to identify patterns.")
+print("Generalization: The model should work on unseen data.")
+
+print("Improvement Over Time: The more data a model is trained on, the better it performs.")
+print()
+print()
+print("Example:")
+
+print("A recommendation system like Netflix suggests movies based on your viewing history by learning patterns from similar users.")
+print()
+print()
+print("A recommendation system like you tube suggests songs playlist based on our interest")
+
+print()
+print()
+print()
+print("            Types of Machine Learning")
+
+print("ML is broadly categorized into three types:")
+
+print("     A. Supervised Learning")
+
+print("The model is trained on labeled data (input-output pairs).")
+
+print("Used for classification (categorizing into classes) and regression (predicting continuous values).")
+
+print("Example 1: Spam Email Classification")
+print("Example 2:Package Prediction Based on CGPA")
+print("Example 3:House-Price Based on number of rooms and Area in Square unit")
+
+
+print("     B.Unsupervised Learning")
+
+print("The model is trained on unlabeled data and finds hidden patterns.")
+
+print("Used for clustering (grouping similar items) and association (finding relationships).")
+
+print("Example 1: Customer Segmentation")
+
+print("      A retail company groups customers based on their purchase behavior:")
+
+print("              Group 1: Frequent buyers")
+
+print("              Group 2: Occasional buyers")
+
+print("              Group 3: One-time buyers")
+
+print()
+print()
+
+print("        C. Reinforcement Learning")
+
+print("The model learns through rewards and penalties for actions taken in an environment.")
+
+print("Used in robotics, game playing, and self-driving cars.")
+
+print("            Example: AlphaGo (Google's AI playing Go)")
+
+print("              AI learns by playing millions of games, improving over time by learning from past moves.")
