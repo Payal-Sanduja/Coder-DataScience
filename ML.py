b05ccb9deb5a763e52ab257fd02411cd1f929b2d
Payal-Sanduja <payalekta19@gmail.com>
2025-02-25 22:27:23 +0530

    Create ML.py

---
+++ ML.py
@@ -0,0 +1,2 @@
+#Machine learning basics
+Machine Learningprint( (ML) is a subset of artificial intelligence (AI) that enables computers to learn patterns from data and make decisions without explicit programming. It helps automate decision-making processes based on historical data)
